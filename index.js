0977634e9acd78108a730b724be2f68394c07535
realIQ2016 <164403841+realIQ2016@users.noreply.github.com>
2024-03-23 02:48:40 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\m\tWellcome to \'codewithiqra\' -IQ Simple Calculateor\n");
+const answer = await inquirer.prompt([
+    { message: "enter frist number", type: "number", name: "fistNumber" },
+    { message: "enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.fistNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.fistNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.fistNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.fistNumber / answer.secondNumber);
+}
+else {
+    console.log("please select valid opreator");
+}
